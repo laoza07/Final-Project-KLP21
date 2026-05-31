6986e438858624957c179a8bf62aabfe63f1660d
mawaddah13 <mawaddahfl13@gmail.com>
2026-05-31 10:42:52 +0800

    feat: Mengisi file MainApp

--- Areda/app/src/main/java/areda/MainApp.java
+++ Areda/app/src/main/java/areda/MainApp.java
@@ -1,11 +1,28 @@
 package areda;
 
-public class MainApp {
-    public String getGreeting() {
-        return "Hello World!";
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        StackPane root = new StackPane();
+        root.getChildren().add(new Label("Areda Careers"));
+
+        Scene scene = new Scene(root, 950, 650);
+
+        primaryStage.setTitle("Areda Careers");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
+    
 
     public static void main(String[] args) {
-        System.out.println(new MainApp().getGreeting());
+        launch(args);
     }
 }
